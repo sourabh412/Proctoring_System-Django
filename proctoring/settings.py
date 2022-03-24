6f19a238f1f996a6b8baae4fcbc723c63e7cf877
sourabh <Hiremathsourabh12@gmail.com>
2022-03-24 18:55:38 +0530

    pwa

--- proctoring/settings.py
+++ proctoring/settings.py
@@ -186,23 +186,23 @@ PWA_APP_BACKGROUND_COLOR = '#ffffff'
 PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/'
 PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = ''
+PWA_APP_START_URL = 'https://bmsce-ise-proc.herokuapp.com/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
 	{
-		'src': 'static/other/BMS-logo.png',
+		'src': 'https://bmsce-ise-proc.herokuapp.com/static/other/BMS-logo.png',
 		'sizes': '160x160'
 	}
 ]
 PWA_APP_ICONS_APPLE = [
 	{
-		'src': 'static/other/BMS-logo.png',
+		'src': 'https://bmsce-ise-proc.herokuapp.com/static/other/BMS-logo.png',
 		'sizes': '160x160'
 	}
 ]
 PWA_APP_SPLASH_SCREEN = [
 	{
-		'src': 'static/other/BMS-logo.png',
+		'src': 'https://bmsce-ise-proc.herokuapp.com/static/other/BMS-logo.png',
 		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
 	}
 ]
